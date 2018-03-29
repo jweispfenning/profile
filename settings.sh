513b6ecbd4564ce44f0df0399515df056cb1ebb7
John Weispfenning <john.weispfenning@jumpcloud.com>
2018-03-28 20:17:47 -0600

    Script updates.

--- settings.sh
+++ settings.sh
@@ -20,14 +20,19 @@ if [ ! -z $EXPORT ]; then
 	cp .bashrc ~/.bashrc;
 	cp .bash_profile ~/.bash_profile;
 	cp .vimrc ~/.vimrc
-	cp .config/nvim/init.vim ~/.config/nvim/init.vim;
+	cp -r .config/ ~/;
 	cp .gitconfig ~/.gitconfig;
 elif [ ! -z $IMPORT ]; then
 	cp ~/.bashrc .bashrc;
 	cp ~/.bash_profile .bash_profile;
 	cp ~/.vimrc .vimrc
 	cp ~/.config/nvim/init.vim .config/nvim/init.vim;
+	cp ~/.config/nvim/after/ .config/nvim/after;
 	cp ~/.gitconfig .gitconfig;
+	mkdir ~/.vim
+	ln -s ~/.config/nvim/swapfiles ~/.vim/swapfiles
+	ln -s ~/.config/nvim/backups ~/.vim/backups
+	ln -s ~/.config/nvim/colors ~/.vim/colors
 else 
 	echo "need an option";
 fi
